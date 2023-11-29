4a2df987067ea837d6e78f227899d536c6812c66
unknown <alotetahihi@gmail.com>
2023-11-29 14:05:28 +0700

    end ss4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,7 +2,7 @@ import Catalog from "../../features/catalog/Catalog"
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material"
 import Header from "./Header"
 import { useState } from "react";
-
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -11,7 +11,7 @@ function App() {
     palette: {
       mode: paletteType,
       background: {
-        default: paletteType === 'light' ? '#eaeaea' : '121212'
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
       }
     }
   })
@@ -26,7 +26,7 @@ function App() {
         <CssBaseline />
         <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
         <Container>
-          <Catalog />
+          <Outlet />
         </Container>
       </ThemeProvider>
   )
