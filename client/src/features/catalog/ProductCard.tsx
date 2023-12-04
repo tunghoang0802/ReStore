e68d50b52dcd70be5676b1c81a108c7cd18a89f4
unknown <alotetahihi@gmail.com>
2023-12-04 18:06:26 +0700

    quao

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,43 +1,57 @@
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { Product } from "../../app/models/product";
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import agent from "../../app/api/agent";
+import { LoadingButton } from "@mui/lab";
+import { useStoreContext } from "../../app/context/StoreContext";
+import { currencyFormat } from "../../app/util/util";
 interface Props {
-    product: Product;
+  product: Product;
 }
 
-export default function ProductCard({product}: Props){
-    return (
-      <Card sx={{ maxWidth: 345 }}>
+export default function ProductCard({ product }: Props) {
+  const [loading, setLoanding] = useState(false);
+  const {setBasket} = useStoreContext();
+  function handleAddItem(productId: number){
+    setLoanding(true);
+    agent.Basket.addItem(productId)
+      .then(basket => setBasket(basket))
+      .catch(error => console.log(error))
+      .finally((() => setLoanding(false)));
 
-        <CardHeader
-          avatar = {
-            <Avatar sx={{bgcolor : 'secondary.main'}}>
-              {product.name.charAt(0).toUpperCase()}
-            </Avatar>
-          }
-          title = {product.name}
-          titleTypographyProps={{
-            sx: {fontWeight: 'bold', color : 'primary.main'}
-          }}
-        />
+  }
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: 'secondary.main' }}>
+            {product.name.charAt(0).toUpperCase()}
+          </Avatar>
+        }
+        title={product.name}
+        titleTypographyProps={{
+          sx: { fontWeight: 'bold', color: 'primary.main' }
+        }}
+      />
       <CardMedia
         sx={{ height: 140, backgroundSize: 'contain' }}
         image={product.pictureUrl}
         title={product.name}
       />
       <CardContent>
-        <Typography gutterBottom color = 'secondary' variant="h5">
-          ${(product.price / 100).toFixed(2)}
+        <Typography gutterBottom color='secondary' variant="h5">
+          {currencyFormat(product.price)}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {product.brand} / {product.type}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Add to cart</Button>
+        <LoadingButton  loading = {loading} onClick={() => handleAddItem(product.id)} size="small">Add to cart</LoadingButton>
         <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
       </CardActions>
     </Card>
-    )
+  )
 }
